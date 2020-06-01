0d2a48950cc95f79a9b832fbcd827fcc6bdc9826
Artur <artarmmush@gmail.com>
2020-06-01 12:39:23 +0400

    Fix test.

--- src/__test__/grid.test.ts
+++ src/__test__/grid.test.ts
@@ -1,10 +1,17 @@
 // tslint:disable-next-line: no-reference
 /// <reference path="../../node_modules/phaser-ce/typescript/phaser.d.ts" />
 // tslint:disable: ordered-imports
-import './phaser';
 import { Phaser2Grid } from '../com/koreez/phaser2grid/Phaser2Grid';
+import './phaser';
 
-class TestGrid extends Phaser2Grid {}
+class TestGrid extends Phaser2Grid {
+  getGridConfig() {
+    return {
+      name: '',
+      bounds: () => ({ x: 0, y: 0, width: 0, height: 0 }),
+    };
+  }
+}
 
 test('Phaser2 Grid', done => {
   function create(this: Phaser.State) {
